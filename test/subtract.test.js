dbe9fc57fe80faaa4c880eb4330e0ece31e14b3a
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-05-11 17:11:14 -0500

    add test for clone, sum, and subtract

---
+++ test/subtract.test.js
@@ -0,0 +1,5 @@
+const subtract = require('../src/subtract')
+
+test('subtract two numbers', () => {
+    expect(subtract(1,2)).toBe(-1)
+})
---
+++ test/sum.test.js
@@ -0,0 +1,5 @@
+const sum = require('../src/sum');
+
+test('test a sum', () => {
+    expect(sum(1,2)).toBe(3)
+})
